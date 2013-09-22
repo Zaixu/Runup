644904d9b107d3ee2b90ef5d6c20c9715ee129fc
Zaixu <nicolajq@gmail.com>
2013-09-22 16:53:10 +0200

    Added Base Project - Windows Phone

---
+++ WindowsPhone/Runup/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Runup.Resources;
+
+namespace Runup
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
