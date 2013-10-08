5ccec321665c84e7e46ef180dd1d882cf4236e38
Zaixu <nicolajq@gmail.com>
2013-10-08 22:05:10 +0200

    Windows service added

--- CloudService/CloudService/Service.cs
+++ CloudService/CloudService/Service.cs
@@ -4,27 +4,34 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using CloudService.Database;
 
 namespace CloudService
 {
     public class Service : IService
     {
-        public bool Login(Database.Users user)
+        public bool Login(Users user)
+        {
+            DatabaseEntities db = new DatabaseEntities();
+            var eu = db.Users.Find(user.Email);
+
+            if (eu != null && eu.Password == user.Password)
+                return true;
+            else
+                return false;
+        }
+
+        public bool Register(Users user)
         {
             throw new NotImplementedException();
         }
 
-        public bool Register(Database.Users user)
+        public bool SaveData(Users user)
         {
             throw new NotImplementedException();
         }
 
-        public bool SaveData(Database.Users user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool LoadData(Database.Users user)
+        public bool LoadData(Users user)
         {
             throw new NotImplementedException();
         }
---
+++ CloudService/WindowsCloudService/Service.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WindowsCloudService
+{
+    public partial class Service : ServiceBase
+    {
+        ServiceHost sHost;
+        public Service()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            sHost = new ServiceHost(typeof(CloudService.Service));
+            sHost.Open();
+        }
+
+        protected override void OnStop()
+        {
+            sHost.Close();
+        }
+    }
+}
