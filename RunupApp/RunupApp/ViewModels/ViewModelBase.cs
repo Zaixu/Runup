7b959b9d863b98128a9fd43e73f5e168b6a191ef
René H. Thomsen <rene_thomsen@yahoo.com>
2013-10-10 20:12:44 +0200

    ViewModel tool classes

---
+++ RunupApp/RunupApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace RunupApp.ViewModels
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
