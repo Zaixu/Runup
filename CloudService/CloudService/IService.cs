6a971c4b02067e01bb94f00c26076a166a9e294b
Zaixu <nicolajq@gmail.com>
2013-10-13 23:24:07 +0200

    Commented my shit

--- CloudService/CloudService/IService.cs
+++ CloudService/CloudService/IService.cs
@@ -10,12 +10,25 @@ using CloudService.Database;
 namespace CloudService
 {
  
+    /// <summary>
+    /// Service interface for outwards communication to applications, defines service and operation contracts for accessing
+    /// </summary>
     [ServiceContract]
     public interface IService
     {
+        /// <summary>
+        /// Handles login function from external apps
+        /// </summary>
+        /// <param name="user">User object to be logged in</param>
+        /// <returns>Returns login status string</returns>
         [OperationContract]
         string Login(Users user);
 
+        /// <summary>
+        /// Handles registering function from external apps
+        /// </summary>
+        /// <param name="user">User object to be registered</param>
+        /// <returns>Returns register status string</returns>
         [OperationContract]
         string Register(Users user);
 
--- CloudService/CloudService/Service.cs
+++ CloudService/CloudService/Service.cs
@@ -9,24 +9,74 @@ using System.Collections.ObjectModel;
 
 namespace CloudService
 {
+    /// <summary>
+    /// Class to function as service outwards to application
+    /// </summary>
     public class Service : IService
     {
+        /// <summary>
+        /// Checks if a user exists
+        /// </summary>
+        /// <param name="user">User log in details</param>
+        /// <returns>String with text according to status</returns>
         public string Login(Users user)
         {
+            //Check the object given
+            string response = Validate(user);
+            //If errors return
+            if (response.Length > 0)
+                return response;
+
+            //Find user email in database
             DatabaseEntities db = new DatabaseEntities();
             var existUser = db.Users.Find(user.Email);
 
+            //If it exists and password is correct, success else return error
             if (existUser != null && existUser.Password == user.Password)
                 return "Success";
             else
-                return "Error logging in";
+                return "User does not exist or wrong info";
         }
 
+        /// <summary>
+        /// Validation of parameter user fields
+        /// </summary>
+        /// <param name="user">User class to be checked</param>
+        /// <returns>String with checks that dint go through</returns>
+        private string Validate(Users user)
+        {
+            string response = "";
+            //Check that email isnt empty
+            if (user.Email.Length == 0)
+                response = response + "Invalid email length\n\r";
+            //Check that password isnt empty
+            if (user.Password.Length == 0)
+                response = response + "Invalid password length\n\r";
+            //check there is atleast a @ and a dot
+            if ((!user.Email.Contains('@')) && user.Email.Contains('.'))
+                response = response + "Invalid email address\n\r";
+            //Return errors
+            return response;
+        }
+
+        /// <summary>
+        /// Registers a user
+        /// </summary>
+        /// <param name="user">User parameter to register</param>
+        /// <returns>Returns a string with error or if succeeded</returns>
         public string Register(Users user)
         {
+            //Check the object given for errors
+            string response = Validate(user);
+            //If errors return
+            if (response.Length > 0)
+                return response;
+
+            //Find user in database from given user data
             DatabaseEntities db = new DatabaseEntities();
             var existUser = db.Users.Find(user.Email);
 
+            //If he doesnt exist, add new user to database, else return error string
             if(existUser == null)
             {
                 db.Users.Add(user);
@@ -35,7 +85,7 @@ namespace CloudService
             }
             else
             {
-                return "Error registering";
+                return "Error already exists";
             }
         }
 
